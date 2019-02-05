870e5dbc57e5db115b43ddba185a7065ddbd9e4a
daremove <daremove1@gmail.com>
2019-02-05 23:36:25 +0500

    add test bubbles

---
+++ src/components/bubble.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bubble" :class="{
+    'animate-original': animation === 'original',
+    'animate-alternate': animation === 'alternate',
+    'animate-alternate-reverse': animation === 'alternate-reverse'
+    }" :style="{height: size, width: size}">
+
+    </div>
+</template>
+<script>
+    export default  {
+        data() {
+            return {
+                animation: '',
+                size: '200px'
+            }
+        }
+    }
+</script>
--- src/components/list.vue
+++ src/components/list.vue
@@ -1,18 +1,50 @@
 <template>
     <transition name="list">
-        <div class="list"></div>
+        <div class="list">
+
+            <bubble />
+            <bubble />
+            <bubble />
+            <bubble />
+            <bubble />
+            <bubble />
+            <bubble />
+            <bubble />
+
+        </div>
     </transition>
 </template>
 <script>
-    export default {
+    import Bubble from './bubble.vue';
 
+    export default {
+        components: {
+            'bubble': Bubble,
+        },
+        data() {
+            return {
+                bubbles: [],
+                animation: ['original', 'alternate', 'alternate-reverse'],
+                sizes: ['100px', '200px', '300px', '150px', '170px']
+            }
+        },
+        created() {
+            function randomValue(arr) {
+                return arr[Math.floor(Math.random() * arr.length)];
+            }
+
+            this.$nextTick(() => {
+                this.bubbles = this.$children;
+
+                this.bubbles.forEach(bubble => {
+                    let animation = randomValue(this.animation);
+                    let size = randomValue(this.sizes);
+
+                    bubble.animation = animation;
+                    bubble.size = size;
+                });
+            });
+
+        }
     }
 </script>
-<style>
-    .list {
-        width: 100%;
-        height: 100%;
-
-        border: 2px solid black;
-    }
-</style>
